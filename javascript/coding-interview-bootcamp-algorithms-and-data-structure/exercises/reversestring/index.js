18b2d991f5476335a2496788e2ac546031834946
Temple Okosun <okosun.temple@gmail.com>
2021-08-31 06:00:35 -0400

    completed

--- javascript/coding-interview-bootcamp-algorithms-and-data-structure/exercises/reversestring/index.js
+++ javascript/coding-interview-bootcamp-algorithms-and-data-structure/exercises/reversestring/index.js
@@ -7,14 +7,13 @@
 //   reverse('Greetings!') === '!sgniteerG'
 
 function reverse(str) {
-    // Take starting value-'', pass it in as 1st arg- rev & use the result of computation as starting point for next run
-    return str.split('').reduce((rev, char) => char + rev, '')
+  // Take starting value-'', pass it in as 1st arg- rev & use the result of computation as starting point for next run
+  return str.split('').reduce((rev, char) => char + rev, '')
 }
 
 console.log(reverse('Love'))
 
-module.exports = reverse;
-
+module.exports = reverse
 
 // Solution 1
 // return str.split('').reverse().join('')
