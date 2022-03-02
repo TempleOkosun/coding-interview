e81f83c20eb60b074854ca1c3f35e3af4713d79a
Temple Okosun <okosun.temple@gmail.com>
2022-03-02 16:50:13 -0500

    tower of Hanoi

--- javascript/LeetCode-with-JavaScript-and-Data-Structures/main.js
+++ javascript/LeetCode-with-JavaScript-and-Data-Structures/main.js
@@ -161,3 +161,18 @@ const getSubsequence = (word) => {
 
   return result.substring(1)
 }
+
+// 7. Tower of Hanoi
+// Solution 1
+
+const towerOfHanoi = (n, fromRod, toRod, auxRod) => {
+  if (n === 1) {
+    console.log('Moving the disk 1 from ' + fromRod + ' to the ' + toRod)
+  } else {
+    towerOfHanoi(n - 1, fromRod, auxRod, toRod)
+    console.log('Moving the ' + n + 'th disk from ' + fromRod + ' to the ' + toRod)
+    towerOfHanoi(n - 1, auxRod, toRod, fromRod)
+  }
+}
+
+towerOfHanoi(4, 'X', 'Y', 'Z')
