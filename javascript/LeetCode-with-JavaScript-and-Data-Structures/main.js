46e19ab557204bc98962e0c7c4345cd5e7714ec1
Temple Okosun <okosun.temple@gmail.com>
2022-03-02 09:00:02 -0500

    Get all substrings of a given string

--- javascript/LeetCode-with-JavaScript-and-Data-Structures/main.js
+++ javascript/LeetCode-with-JavaScript-and-Data-Structures/main.js
@@ -141,3 +141,23 @@ const lengthOfLongestSubstring = (s) => {
   }
   return max
 }
+
+// 6. Return subsequences in a word
+// Solution 1
+/**
+ * @param {string} word
+ *  @return {string}
+ *  */
+const getSubsequence = (word) => {
+  if (word === '') return ''
+  const first = word.charAt(0)
+  const rest = getSubsequence(word.substring(1))
+  let result = ''
+
+  rest.split(',').forEach((subseq) => {
+    result += ',' + first + subseq
+    result += ',' + subseq
+  })
+
+  return result.substring(1)
+}
