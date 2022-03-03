96906a70e4816d288fa590d84cf4c4af8549aab3
Temple Okosun <okosun.temple@gmail.com>
2022-03-02 20:12:24 -0500

    solution to mean median mode

--- javascript/LeetCode-with-JavaScript-and-Data-Structures/main.js
+++ javascript/LeetCode-with-JavaScript-and-Data-Structures/main.js
@@ -275,3 +275,73 @@ const reverseWord = (string) => {
   })
   return reversedWordsArr.join(' ')
 }
+
+// 11. Reverse array in place
+// Solution 1
+const reverseArrayInPlace = (arr) => {
+  for (let i = 0; i < arr.length / 2; i++) {
+    let tempVar = arr[i]
+    arr[i] = arr[arr.length - 1 - i] // the counterpart on the ending side
+    arr[arr.length - 1 - i] = tempVar
+  }
+  return arr
+}
+
+// 12. Mean Median and Mode
+// Solution 1
+const getMean = (arr) => {
+  let sum = 0
+  arr.forEach((num) => {
+    sum += num
+  })
+
+  return sum / arr.length
+}
+
+const getMedian = (arr) => {
+  arr.sort((a, b) => a - b)
+  let median
+  if (arr.length % 2 !== 0) {
+    median = arr[Math.floor(arr.length / 2)]
+  } else {
+    const mid1 = arr[arr.length / 2 - 1]
+    const mid2 = arr[arr.length / 2]
+    median = (mid1 + mid2) / 2
+  }
+  return median
+}
+
+const getMode = (arr) => {
+  let modeObj = {}
+  arr.forEach((num) => {
+    if (!modeObj[num]) {
+      modeObj[num] = 0
+    }
+    modeObj[num]++
+  })
+
+  let maxFrequency = 0
+  let modes = []
+  for (const num in modeObj) {
+    if (modeObj[num] > maxFrequency) {
+      modes = [num]
+      maxFrequency = modeObj[num]
+    } else if (modeObj[num] === maxFrequency) {
+      modes.push(num)
+    }
+  }
+
+  if (modes.length === Object.keys(modeObj).length) {
+    modes = []
+  }
+
+  return modes
+}
+
+const meanMedianMode = (arr) => {
+  return {
+    mean: getMean(arr),
+    median: getMedian(arr),
+    mode: getMode(arr),
+  }
+}
