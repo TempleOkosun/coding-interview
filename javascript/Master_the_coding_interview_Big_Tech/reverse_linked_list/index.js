690e1dd95baadbfc544cbc60b4ee36592a9e3dd9
Temple Okosun <okosun.temple@gmail.com>
2022-03-09 18:22:44 -0500

    singly linked list checkpoint

--- javascript/Master_the_coding_interview_Big_Tech/reverse_linked_list/index.js
+++ javascript/Master_the_coding_interview_Big_Tech/reverse_linked_list/index.js
@@ -1,4 +1,17 @@
 // Reverse a linked list
 // We will be giving the head node
 
-const reverseLinkedList = (head) => {}
+const reverseLinkedList = (head) => {
+  let prev = null
+  let current = head
+
+  while (current) {
+    let temp = current.next
+
+    // change direction
+    current.next = prev
+    prev = current
+    current = temp
+  }
+  return prev
+}
