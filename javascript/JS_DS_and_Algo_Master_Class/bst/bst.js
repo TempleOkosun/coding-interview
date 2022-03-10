7d5c82bf639ee2d1806c2dbb51d742af80af9fd3
Temple Okosun <okosun.temple@gmail.com>
2022-03-09 19:40:21 -0500

    bst find

--- javascript/JS_DS_and_Algo_Master_Class/bst/bst.js
+++ javascript/JS_DS_and_Algo_Master_Class/bst/bst.js
@@ -36,6 +36,23 @@ class BinarySearchTree {
       }
     }
   }
+
+  find(value) {
+    if (this.root === null) return false
+    let current = this.root,
+      found = false
+    while (current && !found) {
+      if (value < current.value) {
+        current = current.left
+      } else if (value > current.value) {
+        current = current.right
+      } else {
+        found = true
+      }
+    }
+    if (!found) return undefined
+    return current
+  }
 }
 
 module.exports = { Node, BinarySearchTree }
