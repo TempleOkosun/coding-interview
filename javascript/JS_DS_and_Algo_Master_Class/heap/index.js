adcc9f10bf0212efcf126b5b16afd86228562dfc
Temple Okosun <okosun.temple@gmail.com>
2022-03-11 19:34:06 -0500

    extract max from binary heap

--- javascript/JS_DS_and_Algo_Master_Class/heap/index.js
+++ javascript/JS_DS_and_Algo_Master_Class/heap/index.js
@@ -20,6 +20,46 @@ class MaxBinaryHeap {
       idx = parentIdx
     }
   }
+
+  extractMax() {
+    const max = this.values[0]
+    const end = this.values.pop()
+    this.values[0] = end
+    // bubble down
+    this.sinkDown()
+
+    const sinkDown = () => {
+      let idx = 0
+      const length = this.values.length
+      const element = this.values[0]
+      while (true) {
+        let leftChildIdx = 2 * idx + 1
+        let rightChildIdx = 2 * idx + 2
+        let leftChild, rightChild
+        let swap = null
+
+        if (leftChild < length) {
+          leftChild = this.values[leftChildIdx]
+          if (leftChild > element) {
+            swap = leftChildIdx
+          }
+        }
+        if (rightChildIdx < length) {
+          rightChild = this.values[rightChildIdx]
+          if (
+            (swap === null && rightChild > element) ||
+            (swap !== null && rightChild > leftChild)
+          ) {
+            swap = rightChildIdx
+          }
+        }
+        if (swap === null) break
+        this.values[idx] = this.values[swap]
+        this.values[swap] = element
+        idx = swap
+      }
+    }
+  }
 }
 
 let heap = new MaxBinaryHeap()
