71689f14ba94ef725d215c2479f1b6cb8d25a208
Temple Okosun <okosun.temple@gmail.com>
2022-04-28 03:33:59 -0400

    graphs - remove vertex

--- javascript/JS_DS_and_Algo_Master_Class/graph/Graph.js
+++ javascript/JS_DS_and_Algo_Master_Class/graph/Graph.js
@@ -16,4 +16,12 @@ class Graph {
     this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter((v) => v !== vertex2)
     this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter((v) => v !== vertex1)
   }
+
+  removeVertex(vertex) {
+    while (this.adjacencyList[vertex].length) {
+      const adjacencyVertex = this.adjacencyList[vertex].pop()
+      this.removeEdge(vertex, adjacencyVertex)
+    }
+    delete this.adjacencyList[vertex]
+  }
 }
