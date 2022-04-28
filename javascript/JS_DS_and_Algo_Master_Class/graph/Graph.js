2fac298a0596dbfab6ccd42a43a319abb0436eec
Temple Okosun <okosun.temple@gmail.com>
2022-04-28 11:02:27 -0400

    graphs - depth first traversal for graphs

--- javascript/JS_DS_and_Algo_Master_Class/graph/Graph.js
+++ javascript/JS_DS_and_Algo_Master_Class/graph/Graph.js
@@ -24,4 +24,41 @@ class Graph {
     }
     delete this.adjacencyList[vertex]
   }
+
+  depthFirstRecursive(start) {
+    const result = []
+    const visited = {}
+    const adjacencyList = this.adjacencyList(function dfs(vertex) {
+      if (!vertex) return null
+      visited[vertex] = true
+      result.push(vertex)
+      adjacencyList[vertex].forEach((neighbour) => {
+        if (!visited[neighbour]) {
+          return dfs(neighbour)
+        }
+      })
+    })(start)
+    return result
+  }
+
+  depthFirstIterative(start) {
+    const stack = [start]
+    const result = []
+    const visited = {}
+    let currentVertex
+
+    visited[start] = true
+    while (stack.length) {
+      currentVertex = stack.pop()
+      result.push(currentVertex)
+
+      this.adjacencyList[currentVertex].forEach((neighbour) => {
+        if (!visited[neighbour]) {
+          visited[neighbour] = true
+          stack.push(neighbour)
+        }
+      })
+    }
+    return result
+  }
 }
